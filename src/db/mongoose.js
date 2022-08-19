cdbb702cd4a363d39141823eb77c94136f0bf7ba
Ayan Sadhu <ronyee45@gmail.com>
2022-08-19 13:29:47 +0530

    change db to atlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const Task = require('../models/task')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+//mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb+srv://ayansadhu:1qaz2wsx@cluster0.dxmnhxk.mongodb.net/?retryWrites=true&w=majority',{
     // useNewUrlParser: true,
-    // useCreateIndex: true
+    // useCreateIndex: true,
+    writeConcern: { w: 'majority', j: true, wtimeout: 1000 }
 })
 
 
